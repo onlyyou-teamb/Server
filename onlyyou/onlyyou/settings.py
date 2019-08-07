fa31c926627e08b9bc3651a37a99c164ecb8f389
HyunjaeLee <leehj8687@gmail.com>
2019-08-07 16:29:48 -0700

    Add super user and basic templates

--- onlyyou/onlyyou/settings.py
+++ onlyyou/onlyyou/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Third party
+    'main',
+
+    # Base
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Third party
-    'main',
 ]
 
 MIDDLEWARE = [
