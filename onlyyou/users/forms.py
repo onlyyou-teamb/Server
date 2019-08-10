f6db5db45ff56c8be9d32e69bb375ab93aa2bdad
sjly3k <40621448+sjly3k@users.noreply.github.com>
2019-08-10 09:46:11 +0900

    2019-08-09 profile form, model dev
    2019-08-09 profile form, model dev

--- onlyyou/users/forms.py
+++ onlyyou/users/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['first_image', 'second_image', 'third_image']
--- onlyyou/users/models.py
+++ onlyyou/users/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+import os
 
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/profile/{1}'.format(instance.user.username, filename)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    first_image = models.FileField(default='first_image.jpg', upload_to=user_directory_path)
+    second_image = models.FileField(default='second_image.jpg', upload_to=user_directory_path)
+    third_image = models.FileField(default='third_image.jpg', upload_to=user_directory_path)
+    #upload = models.FileField(upload_to=user_directory_path)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
     def save(self):
         super().save()
+        #
+        # img_1 = Image.open(self.first_image.path)
+        # img_1.save(first_image. .path)
+        # img_2 = Image.open(self.second_image.path)
+        # img_2.save(img_2.path)
+        # img_3 = Image.open(self.third_image.path)
+        # img_3.save(img_3.path)
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
