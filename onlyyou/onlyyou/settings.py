7ac6524f8c21020f637070aa093db8dcb164c1fe
HyunjaeLee <leehj8687@gmail.com>
2019-08-14 13:56:22 -0700

    Docker & Docker Compose with Mysql

--- onlyyou/onlyyou/settings.py
+++ onlyyou/onlyyou/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'onlyyou.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlyou-db',
+        'USER': 'onlyyou',
+        'PASSWORD': 'teamb',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
